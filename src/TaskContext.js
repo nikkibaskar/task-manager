f52a9f22ec17c9e551199971e2ff463471d5118e
NIKHIL BASKARAN <niklbas1999@gmail.com>
2024-11-18 11:42:09 +0530

    first commit

---
+++ src/TaskContext.js
@@ -0,0 +1,15 @@
+import { createContext, useReducer } from 'react';
+import { initialState, taskReducer } from './TaskReducer';
+
+export const TaskContext = createContext();
+
+export const TaskProvider = ({children}) => {
+
+    const [state, dispatch] = useReducer(taskReducer, initialState);
+    
+    return (
+        <TaskContext.Provider value={{state, dispatch}}>
+            {children}
+        </TaskContext.Provider>
+    );
+};
